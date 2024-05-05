cddb1980f9398b5303b3b996b5d8c4f349bcee97
Banti Patel <bantipatel2506@gmail.com>
2024-05-05 16:27:57 +0530

    add data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import Login from './Ui/pages/Login/Login'
-import Router from './Router/Router'
-import { ToastContainer } from 'react-toastify'
+import { useState } from "react";
+import Router from "./Router/Router";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-
   return (
     <>
       <Router />
       <ToastContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Profile from '../Ui/pages/Profile/Profile'
-import Login from '../Ui/pages/Login/Login'
-import Signup from '../Ui/pages/Signup/Signup'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../Components/Pages/Login/Login";
+import Profile from "../Components/Pages/Profile/Profile";
+import Signup from "../Components/Pages/Signup/Signup";
 
 export default function Router() {
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/profile" element={<Profile />} />
-                    <Route path="/signup" element={<Signup />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    )
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
