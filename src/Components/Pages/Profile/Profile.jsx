30c14f668cce167a321baa63d58b913a1db1f014
Banti Patel <bantipatel2506@gmail.com>
2024-05-08 17:49:48 +0530

    add data

--- src/Components/Pages/Profile/Profile.jsx
+++ src/Components/Pages/Profile/Profile.jsx
@@ -11,14 +11,10 @@ export default function Profile() {
 
     if (userDataString) {
       const userData = JSON.parse(userDataString);
-      console.log("userData:", userData);
 
       const { data } = userData;
-      console.log("data:", data);
 
       const { followers, following, postCount } = data;
-      console.log("followers:", followers);
-      console.log("following:", following);
 
       if (followers !== undefined) {
         setFollowersCount(followers);
@@ -35,60 +31,62 @@ export default function Profile() {
   }, []);
 
   return (
-    <div className="profile">
+    <div>
       <Option />
-      <div className="profile-header">
-        <img src="user.jpg" alt="Profile Picture" />
-        <div className="profile-info">
-          <div className="profile-actions">
-            <h1>Username</h1>
-            <button>Edit Profile</button>
-            <button>View Archive</button>
-            <button className="settings-button">&#9881;</button>
+      <div className="profile">
+        <div className="profile-header">
+          <img src="user.jpg" alt="Profile Picture" />
+          <div className="profile-info">
+            <div className="profile-actions">
+              <h1>Username</h1>
+              <button>Edit Profile</button>
+              <button>View Archive</button>
+              <button className="settings-button">&#9881;</button>
+            </div>
+            <div className="follow-stats">
+              <span>Posts: {postCount}</span>
+              <span>Followers: {followersCount}</span>
+              <span>Following: {followingCount}</span>
+            </div>
+            <p className="bio">Bio</p>
           </div>
-          <div className="follow-stats">
-            <span>Posts: {postCount}</span>
-            <span>Followers: {followersCount}</span>
-            <span>Following: {followingCount}</span>
+        </div>
+        <div className="tab-list">
+          <div className="tab">
+            <span className="icon">&#128247;</span>
+            <span>Posts</span>
           </div>
-          <p className="bio">Bio</p>
+          <div className="tab">
+            <span className="icon">&#128465;</span>
+            <span>Saved</span>
+          </div>
+          <div className="tab">
+            <span className="icon">&#64;</span>
+            <span>Tagged</span>
+          </div>
         </div>
-      </div>
-      <div className="tab-list">
-        <div className="tab">
-          <span className="icon">&#128247;</span>
-          <span>Posts</span>
-        </div>
-        <div className="tab">
-          <span className="icon">&#128465;</span>
-          <span>Saved</span>
-        </div>
-        <div className="tab">
-          <span className="icon">&#64;</span>
-          <span>Tagged</span>
-        </div>
-      </div>
-      <div className="posts-grid">
-        <div className="post">
-          <img src="post.jpg" alt="Post Image" />
-        </div>
-        <div className="post">
-          <img src="post.jpg" alt="Post Image" />
-        </div>
-        <div className="post">
-          <img src="post.jpg" alt="Post Image" />
-        </div>
-        <div className="post">
-          <img src="post.jpg" alt="Post Image" />
-        </div>
-        <div className="post">
-          <img src="post.jpg" alt="Post Image" />
-        </div>
-        <div className="post">
-          <img src="post.jpg" alt="Post Image" />
-        </div>
-        <div className="post">
-          <img src="post.jpg" alt="Post Image" />
+        <div className="posts-grid">
+          <div className="post">
+            <img src="post.jpg" alt="Post Image" />
+          </div>
+          <div className="post">
+            <img src="post.jpg" alt="Post Image" />
+          </div>
+          <div className="post">
+            <img src="post.jpg" alt="Post Image" />
+          </div>
+          <div className="post">
+            <img src="post.jpg" alt="Post Image" />
+          </div>
+          <div className="post">
+            <img src="post.jpg" alt="Post Image" />
+          </div>
+          <div className="post">
+            <img src="post.jpg" alt="Post Image" />
+          </div>
+          <div className="post">
+            <img src="post.jpg" alt="Post Image" />
+          </div>
         </div>
       </div>
     </div>
